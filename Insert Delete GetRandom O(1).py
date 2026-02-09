18f91eaec4a83e3f84e7b0a91eb6be0e7ae2fc81
yafaaa <131185124+yafaaa@users.noreply.github.com>
2026-02-09 14:05:31 +0300

    Create Insert Delete GetRandom O(1).py

---
+++ Insert Delete GetRandom O(1).py
@@ -0,0 +1,31 @@
+class RandomizedSet:
+
+    def __init__(self):
+        self.sets = set()        
+
+    def insert(self, val: int) -> bool:
+        if val not in self.sets:
+            self.sets.add(val)
+            return True
+        return False
+        
+
+    def remove(self, val: int) -> bool:
+        if val in self.sets:
+            self.sets.remove(val)
+            return True
+        else:
+            return False
+
+
+    def getRandom(self) -> int:
+         choice = random.randint(0, len(self.sets) - 1)
+         return list(self.sets)[choice]
+
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
