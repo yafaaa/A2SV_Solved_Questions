c071eeec27446fbc55e92fcbbb7b12db0d6557ba
yafaaa <131185124+yafaaa@users.noreply.github.com>
2026-02-12 11:46:11 +0300

    Create Determine if Two Strings Are Close.py

---
+++ Determine if Two Strings Are Close.py
@@ -0,0 +1,12 @@
+from collections import Counter
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        if (len(word1)!= len(word2)):
+            return False
+        if word1 =="":
+            return True
+        d1 = Counter(word1)
+        d2 = Counter(word2)
+        if set(d1.keys()) != set(d2.keys()):
+            return False
+        return sorted(d1.values()) == sorted(d2.values())
