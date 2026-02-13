0228bfe0d4bcbc934e6cd8955ac25270e7113193
yafaaa <131185124+yafaaa@users.noreply.github.com>
2026-02-13 09:18:41 +0300

    Update Minimum Swaps to Make Strings Equal.py

--- Minimum Swaps to Make Strings Equal.py
+++ Minimum Swaps to Make Strings Equal.py
@@ -1,31 +1,19 @@
 class Solution:
     def minimumSwap(self, s1: str, s2: str) -> int:
-        d1 = Counter(s1)
-        d2 = Counter(s2)
-        dt = d1.copy()
-        dt.update(s2)
+        cnt_xy, cnt_yx = 0, 0
+        t = 0
         n = len(s1)
-        s1 = list(s1)
-        s2 = list(s2)
-        t = all(a%2 == 0 for a in dt.values())
-        if not t:
+        d = Counter(s1)
+        d.update(s2)
+        if any(v%2 != 0 for k, v in d.items()):
             return -1
-        c = 0
 
         for i in range(n):
             if s1[i] != s2[i]:
-                avail = next((j for j in range(i+1,n) if s2[j] == s2[i] and s1[j] != s2[j]), None)
-                if avail:
-                    s2[avail] = s1[i]
-                    d1[s2[i]] += 1
-                    d1[s1[i]] -= 1
-                    d2[s2[i]] -= 1
-                    d2[s1[i]] += 1
-                    c += 1
-                    
+                if s1[i] == "x":
+                    cnt_xy += 1
                 else:
-                    temp = next((j for j in range(i+1, n) if s2[j] == s1[i] and s1[j] == s2[i]))
-                    s1[temp] = s1[i]
-                    c += 2
-        return c
+                    cnt_yx += 1
         
+        return (cnt_xy//2) + (cnt_yx//2) + (cnt_yx % 2) + (cnt_xy  % 2)
+      
