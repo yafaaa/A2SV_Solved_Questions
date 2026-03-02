58dcefa55360c7d3eea806ae707ecd8e08bf709a
yafaaa <131185124+yafaaa@users.noreply.github.com>
2026-03-02 17:17:15 +0300

    Update Subarrays with K Different Integers.py

--- Subarrays with K Different Integers.py
+++ Subarrays with K Different Integers.py
@@ -1,10 +1,9 @@
 from collections import defaultdict
 class Solution:
-    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
-        n = len(nums)
+    def atmost(self, nums, k):
         d = defaultdict(int)
-        a, cnt_k, cntk_1 = 0, 0, 0
-
+        n = len(nums)
+        a, cnt_k = 0, 0
         for b in range(n):
             d[nums[b]] += 1
             
@@ -14,19 +13,7 @@ class Solution:
                     del d[nums[a]]
                 a += 1
             cnt_k += b-a+1
-        d.clear()
-        a = 0
-        for b in range(n):
-            d[nums[b]] += 1
-            
-            while len(d.keys()) > k-1:
-                d[nums[a]] -= 1
-                if not d[nums[a]]:
-                    del d[nums[a]]
-                a += 1
-            cntk_1 += b-a+1
-        
-        return cnt_k - cntk_1
+        return cnt_k
 
-            
-
+    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        return self.atmost(nums, k) - self.atmost(nums, k-1) 
