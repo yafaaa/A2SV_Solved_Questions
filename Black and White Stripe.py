81f52b15e2a2a77f0b6236f8086df2d734519aa4
yafaaa <131185124+yafaaa@users.noreply.github.com>
2026-03-02 18:47:45 +0300

    Update Black and White Stripe.py

--- Black and White Stripe.py
+++ Black and White Stripe.py
@@ -6,11 +6,11 @@ for _ in range(int(input())):
     cnt_w = string[:k].count("W")
     mn = cnt_w 
     
-    for b in range(1,n-k):
+    for b in range(k,n):
         if string[b] == "W":
             cnt_w += 1
-            mn = min(mn, cnt_w)
         a = b-k+1
         if string[a-1] == "W":
             cnt_w -= 1
+        mn = min(mn, cnt_w)
     print(mn)
