2cc43f19b8de1772d7a2497599248013b13490e7
yafaaa <131185124+yafaaa@users.noreply.github.com>
2026-03-12 19:04:27 +0300

    Create Previous Smaller Element.py

---
+++ Previous Smaller Element.py
@@ -0,0 +1,15 @@
+class Solution:
+	def prevSmaller(self, arr):
+        n = len(arr)
+        stack = []
+        res = []
+        for i in range(n):
+            
+            while stack and arr[stack[-1]] >= arr[i]:
+                stack.pop()
+                
+            res.append(-1 if not stack else arr[stack[-1]])    
+
+            stack.append(i)
+            
+        return res
