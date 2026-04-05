81004b7e69bd16f533d7523937b80e76cdad4957
yafaaa <131185124+yafaaa@users.noreply.github.com>
2026-04-05 18:56:53 +0300

    Update First Bad Version.py

--- First Bad Version.py
+++ First Bad Version.py
@@ -3,13 +3,16 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        def fun(l,r):
-            while l <= r:
-                m = (l+r)//2
-                if isBadVersion(m):
-                    r = m-1
-                else:
-                    l = m+1
-            return l
+        l = 1
+        r = n
+        ans = r
+        while l <= r:
+            m = (l+r)//2
+            if isBadVersion(m):
+                ans = m
+                r = m-1
+            else:
+                l = m+1
+        return ans
 
-        return fun(1,n)
+    
