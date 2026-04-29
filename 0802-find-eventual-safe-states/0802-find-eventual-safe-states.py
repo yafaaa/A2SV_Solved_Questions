484199cba6e9e0853a79bae97d0788dbead7ac7a
yafaaa <131185124+yafaaa@users.noreply.github.com>
2026-04-29 11:25:27 +0300

    Time: 21 ms (92.44%), Space: 28.3 MB (34.27%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -0,0 +1,28 @@
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        n = len(graph)
+        visited = [0]*n
+        ans = []
+
+        def dfs(node):
+            nonlocal ans
+            if visited[node] == 1:
+                return False
+            visited[node] = 1
+            
+            for child in graph[node]:
+                if visited[child] != 2:
+                    if not dfs(child):
+                        return False
+
+            visited[node] = 2
+            ans.append(node)
+            return True
+            
+        
+        for i in range(n):
+            if  not visited[i] :
+                dfs(i)
+            
+        return sorted(ans)
+            
