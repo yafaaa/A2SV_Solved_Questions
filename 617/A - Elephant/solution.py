4d41836a92c35bcfbf0619defd65b635528684cc
yafaaa <131185124+yafaaa@users.noreply.github.com>
2026-05-01 19:55:29 +0300

    Add Elephant [A] from Codeforces

---
+++ 617/A - Elephant/solution.py
@@ -0,0 +1,22 @@
+def solve():
+    x = int(input())
+ 
+    steps = 0
+    steps += x // 5
+    x %= 5
+ 
+    steps += x // 4
+    x %= 4
+ 
+    steps += x // 3
+    x %= 3
+ 
+    steps += x // 2
+    x %= 2
+ 
+    steps += x // 1
+    x %= 1
+ 
+    print(steps)
+ 
+solve()
