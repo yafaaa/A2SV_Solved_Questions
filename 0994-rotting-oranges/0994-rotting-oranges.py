37fe5996e31e25463f34e4926bb1e3f96032a1fb
yafaaa <131185124+yafaaa@users.noreply.github.com>
2026-05-02 16:14:00 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -5,21 +5,29 @@ class Solution:
 
         def inbound(nw_r, nw_c):
             return 0 <= nw_r < len(grid) and 0 <= nw_c < len(grid[0])
-        
-        dq = deque([(r,c) for r in range(len(grid)) for c in range(len(grid[0])) if grid[r][c] == 2])
-        cnt = 0
-        while dq:
+
+        fresh_count = 0
+        dq = deque()
+
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 2:
+                    dq.append((r, c))
+                elif grid[r][c] == 1:
+                    fresh_count += 1
+        time = 0
+        while dq and fresh_count>0:
+            time += 1
             lgth = len(dq)
-            f = False
             for _ in range(lgth):
                 r, c = dq.popleft()
                 for dr, dc in direction:
                     nw_r = r + dr
                     nw_c = c + dc
                     if inbound(nw_r, nw_c) and grid[nw_r][nw_c]==1:
-                        f = True
+                        fresh_count -= 1
                         dq.append((nw_r, nw_c))
                         grid[nw_r][nw_c] = 2
-            cnt += 1 if f else 0
-        return -1 if any(row.count(1) for row in grid) else cnt
+
+        return -1 if fresh_count else time
         
