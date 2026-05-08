d31783830d910f3fe3355ba3d4503719748fe82e
yafaaa <131185124+yafaaa@users.noreply.github.com>
2026-05-08 23:19:20 +0300

    Time: 0 ms (100%), Space: 19.6 MB (20.03%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,7 +1,7 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         d = Counter(words)
-        min_heap = [(v,k) for k, v in d.items()]
+        # min_heap = [(v,k) for k, v in d.items()]
         s = sorted(d.items(), key = lambda x: (-x[1], x[0]))
         ans = []
         for i in range(k):
