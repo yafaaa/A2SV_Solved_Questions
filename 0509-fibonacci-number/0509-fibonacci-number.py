88549fce6f6d55fb671efd7d76a909330918fb88
yafaaa <131185124+yafaaa@users.noreply.github.com>
2026-05-15 17:34:26 +0300

    Time: 46 ms (65.72%), Space: 19.2 MB (57.1%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,8 +1,11 @@
 class Solution:
-    @cache
     def fib(self, n: int) -> int:
-        if n == 1:
-            return 1
-        if n == 0:
-            return 0
-        return self.fib(n-1) + self.fib(n-2)
+        if n == 0: return 0
+        dp = [0] * (n + 1)
+        dp[1] = 1
+
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[n]
+    
