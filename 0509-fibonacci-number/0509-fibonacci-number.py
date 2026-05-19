26c916012229a364c0b8f21676b20a5abfb5a8ad
yafaaa <131185124+yafaaa@users.noreply.github.com>
2026-05-19 16:39:17 +0300

    Time: 659 ms (5.22%), Space: 19.2 MB (57.44%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,10 +1,13 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0: return 0
-        pprev, prev = 0, 1
-        for i in range(2, n+1):
-            cur = prev + pprev
-            pprev = prev
-            prev = cur
-        return prev
-    
+        def fun(n):
+            memo = {}
+            if n == 1:
+                return 1
+            if n == 0:
+                return 0
+            if n in memo:
+                return memo[n]
+            memo[n] = fun(n-1) + fun(n-2)
+            return memo[n]
+        return fun(n)
