17837b251e19b32ac441df12925e6435ab6b122b
yafaaa <131185124+yafaaa@users.noreply.github.com>
2026-05-26 19:00:50 +0300

    Time: 534 ms (92.67%), Space: 34.3 MB (28.17%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.py
+++ 0493-reverse-pairs/0493-reverse-pairs.py
@@ -1,44 +1,30 @@
 class Solution:
     def reversePairs(self, nums: List[int]) -> int:
-            
         def merge(left, right):
-            
+        
             count = 0
             j = 0
             for i in range(len(left)):
                 while j < len(right) and left[i] > 2 * right[j]:
                     j += 1
                 count += j
-            
-            # merge, this can be replaced by return sorted(right + left)
-
-            # merged = []
-            # i = j = 0
-            # while i < len(left) and j < len(right):
-            #     if left[i] <= right[j]:
-            #         merged.append(left[i])
-            #         i += 1
-            #     else:
-            #         merged.append(right[j])
-            #         j += 1
-            # merged.extend(left[i:])
-            # merged.extend(right[j:])
+        
+            # merge
             merged = sorted(left + right)
-            
             return merged, count
-        
-        
-        def merge_sort(nums):
-            if len(nums) <= 1:
-                return nums, 0
+    
+    
+        def merge_sort(l, r):
+            if l == r:
+                return [nums[l]], 0
             
-            mid = len(nums) // 2
-            left, lcnt = merge_sort(nums[:mid])
-            right, rcnt = merge_sort(nums[mid:])
+            mid = (l + r) // 2
+            left, lcnt = merge_sort(l, mid)
+            right, rcnt = merge_sort(mid+1, r)
             merged, count = merge(left, right)
             return merged, lcnt + count + rcnt
             
 
         
-        _, ans = merge_sort(nums)
+        _, ans = merge_sort(0, len(nums)-1)
         return ans
