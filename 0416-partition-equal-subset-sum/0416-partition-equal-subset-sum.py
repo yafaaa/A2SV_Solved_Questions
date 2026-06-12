bc4d3b949679bc7890fd3ba67f14884352684bbd
yafaaa <131185124+yafaaa@users.noreply.github.com>
2026-06-12 20:52:08 +0300

    Time: 694 ms (53.44%), Space: 163.2 MB (15.38%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,29 +1,24 @@
-from typing import List
-from functools import lru_cache
-
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        total_sum = sum(nums)
-        
-        # If the total sum is odd, we cannot split it into two equal integer subsets
-        if total_sum % 2 != 0:
+        n = len(nums)
+        total = sum(nums)
+        if total % 2:
             return False
-            
-        target = total_sum // 2
-        
+        target = total // 2 
         @cache
-        def dfs(i: int, current_target: int) -> bool:
-            # Base Case 1: We found a subset that hits the target perfectly
-            if current_target == 0:
+        def dp(i, curr):
+            
+            if curr == target:
                 return True
-                
-            # Base Case 2: Out of bounds or target went negative
-            if i >= len(nums) or current_target < 0:
+            
+            if curr > target or i == n:
                 return False
-            
-            # Choice 1: Include the current number nums[i]
-            # Choice 2: Exclude the current number nums[i]
-            # If either choice leads to a True, return True
-            return dfs(i + 1, current_target - nums[i]) or dfs(i + 1, current_target)
-            
-        return dfs(0, target)
+                
+            take = dp(i+1, curr+nums[i])
+            if take:
+                return True
+
+            skip = dp(i+1, curr)
+
+            return take or skip
+        return dp(0, 0)
