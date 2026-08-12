6a1ae21408af60a593e6868db55fae12baaa090a
yafaaa <131185124+yafaaa@users.noreply.github.com>
2026-08-12 18:25:11 +0300

    Time: 0 ms (100%), Space: 19.3 MB (10.79%) - LeetHub

--- 0292-nim-game/0292-nim-game.py
+++ 0292-nim-game/0292-nim-game.py
@@ -1,8 +1,6 @@
 class Solution:
     def canWinNim(self, n: int) -> bool:
-        
-        if n < 4:
-            return True
+    
         if not n % 4:
             return False
         return True 
